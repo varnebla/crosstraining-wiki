becd9084b944d8706cc7a0bda623127df241f258
Victor Arnedo Blanco <victor@MacBook-Air-de-Victor.local>
2024-11-13 23:36:35 +0100

    feat: add fuse library, add icons, add tags, add cards, add movement page

---
+++ src/content/config.ts
@@ -0,0 +1,57 @@
+import { defineCollection, z, reference } from 'astro:content';
+
+const movementsCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    thumbnail: z.string(),
+    tags: z.array(z.string()),
+    material: z.array(reference('materialCollection')),
+    category: reference('categoryCollection'),
+    difficulty: z.string(),
+    drills: z.array(z.string()),
+    related: z.array(reference('movementsCollection')),
+    description: z.string(),
+    muscles: z.array(z.string()),
+  }),
+});
+
+const materialCollection = defineCollection({
+  type: 'data',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+  }),
+});
+
+const categoryCollection = defineCollection({
+  type: 'data',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+  }),
+});
+
+const drillsCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    thumbnail: z.string(),
+    slug: z.string(),
+    tags: z.array(z.string()),
+    material: z.array(reference('materialCollection')),
+    category: reference('categoryCollection'),
+    // difficulty: z.string(),
+    // drills: z.array(z.string()),
+    // related: z.array(reference('movementsCollection')),
+    description: z.string(),
+    muscles: z.array(z.string()),
+  }),
+});
+
+export const collections = { 
+  movements: movementsCollection,
+  materials: materialCollection,
+  categories: categoryCollection,
+  drills: drillsCollection
+};
