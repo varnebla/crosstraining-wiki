17e64c9f6405fa1fe1de98867c586bf1560de1ad
Victor Arnedo Blanco <victor@MacBook-Air-de-Victor.local>
2024-11-27 20:11:10 +0100

    feat: add svelte to project, add sitemap module, add robots txt file, add search component, create search logic and show results, rename movements to movimientos for seo reasons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte'
 
 import mdx from '@astrojs/mdx';
 
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()]
+  site: 'https://crosstraining.wiki',
+  integrations: [mdx(), tailwind(), svelte(), sitemap()],
 });
