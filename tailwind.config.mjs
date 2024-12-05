a8fd8c8671df27389f8eb1c526983927801a290f
Victor Arnedo Blanco <victor@MacBook-Air-de-Victor.local>
2024-12-05 01:24:56 +0100

    feat: add new color palette

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,52 @@
 /** @type {import('tailwindcss').Config} */
+const accent = {
+	10: '',
+	20: '',
+	30: '',
+	40: '',
+	50: 'hsl(87 100% 82%)',
+	60: '',
+	70: '',
+	80: '',
+	90: '',
+	95: '',
+}
+
+const secondary = {
+	10: '',
+	20: '',
+	30: '',
+	40: '',
+	50: 'hsl(207 100% 82%)',
+	60: '',
+	70: '',
+	80: '',
+	90: '',
+	95: '',
+}
+
+const tertiary = {
+	10: '',
+	20: '',
+	30: '',
+	40: '',
+	50: 'hsl(327 100% 82%)',
+	60: '',
+	70: '',
+	80: '',
+	90: '',
+	95: '',
+}
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			colors:{
+				accent,
+				secondary,
+				tertiary,
+			},
 			fontFamily: {
 				satoshi: ['Satoshi-Variable', 'sans-serif'],
 				inter: ['Inter Variable', 'sans-serif'],
